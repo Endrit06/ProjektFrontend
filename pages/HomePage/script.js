b7750f566dc75355775ccfd831afa4eeb26884d7
Lend102 <lp73084@ubt-uni.net>
2025-12-17 15:21:05 +0100

    Add some new changes

--- pages/HomePage/script.js
+++ pages/HomePage/script.js
@@ -9,30 +9,21 @@ menuCloseButton.addEventListener("click", () => menuOpenButton.click());
 
 
 const swiper = new Swiper('.slider-wrapper', {
-direction: 'vertical',
+
 loop: true,
-grabCursor: true,
-spaceBetween: 25,
+
 
 pagination: {
-el: ".swiper-pagination",
-clickable: true,
-dynamicBullets: true,
+el: '.swiper-pagination',
+
 },
 
 navigation: {
-nextEl: ".swiper-button-next",
-prevEl: ".swiper-button-prev",
+nextEl: '.swiper-button-next',
+prevEl: '.swiper-button-prev',
 },
+
+
 });
-breakpoints: 
-    0; {
-        slidesPerView: 1
-    }
-     768; {
-        slidesPerView: 2
-}
- 1024; {
-        slidesPerView: 3
+
     
-};
